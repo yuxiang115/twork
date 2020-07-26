e1322249c8ce9729c2e52de6861b50f820f58651
Xiang Yu <yuxiang115@126.com>
2020-07-26 16:07:33 -0700

    test

---
+++ src/main/java/com/xiang/twork/TworkApplication.java
@@ -0,0 +1,13 @@
+package com.xiang.twork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TworkApplication.class, args);
+	}
+
+}
